7c6e78b3ffb3f69a45209ce59268862170460168
Jaron van Well <49275554+JaronVW@users.noreply.github.com>
2022-05-26 11:57:38 +0200

    installed dependencies and set up index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import express from "express";
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`);
+});
